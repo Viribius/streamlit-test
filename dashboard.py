01aad53384751beefee0512cfe4ac083cdb8890b
Viribius <pbri3836@gmail.com>
2026-09-21 15:19:05 +1000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -43,7 +43,7 @@ if not st.session_state.authenticated:
 
     if login:
 
-        if entered_password == st.secrets["password"\]:
+        if entered_password == st.secrets["password"]:
 
             st.session_state.authenticated = True
             st.rerun()
